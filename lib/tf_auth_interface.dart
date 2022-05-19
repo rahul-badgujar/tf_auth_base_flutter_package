ecdd36bcde43b021163d59bdf5a86f1b6f688280
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-19 16:08:03 +0530

    added methods

--- lib/tf_auth_interface.dart
+++ lib/tf_auth_interface.dart
@@ -1,1 +1,22 @@
-abstract class TfAuth {}
+abstract class TfAuth {
+  Future<void> loginWithEmailPassword(
+      {required String email, required String password}) {
+    throw UnimplementedError();
+  }
+
+  Future<void> loginWithEmailLink({required String email}) {
+    throw UnimplementedError();
+  }
+
+  Future<void> loginWithGoogle() {
+    throw UnimplementedError();
+  }
+
+  Future<void> loginWithApple() {
+    throw UnimplementedError();
+  }
+
+  Future<void> loginWithFacebook() {
+    throw UnimplementedError();
+  }
+}
